4301af6ebbe72e13f8aa6c1d2700945a6e3ea860
Dima Shibun <shibun2006@gmail.com>
2025-05-17 17:22:08 +0300

    http://localhost:3000/something-not-exist

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import { createRoot } from "react-dom/client"; // ✅ Используем createRoot
 import "./style/Navbar.css"; // ✅ Подключаем стили глобально
 import "./style/FirstGroup.css";
@@ -19,3 +20,17 @@ const Group = () => <FirstGroup />;
 const root_2 = createRoot(document.getElementById("app")); 
 root_2.render(<Group />);
 
+const Index = () => {
+  useEffect(() => {
+    fetch("http://localhost:8080/api/data")
+      .then(response => response.json())
+      .then(data => {
+        console.log("Полученные данные:", data); // ✅ Вывод в консоль
+      })
+      .catch(error => console.error("Ошибка:", error));
+  }, []);
+
+  return <div>Главная страница</div>;
+};
+
+export default Index;
